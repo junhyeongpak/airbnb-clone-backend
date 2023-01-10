109654246101dc642765b7299c6d280d351914d2
Junhyeong <wnsgudqkr718@gmail.com>
2023-01-11 03:43:31 +0900

    feat : Rest APP ~Pagination

--- categories/serializers.py
+++ categories/serializers.py
@@ -5,5 +5,7 @@ from .models import Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = "__all__"
-        # exclude = ("created_at",)
+        fields = (
+            "name",
+            "kind",
+        )
--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,8 +1,67 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Amenity
+from rest_framework import serializers
+from .models import Amenity, Room
+from users.serializers import TinyUserSerializer
+from categories.serializers import CategorySerializer
+from reviews.serializers import ReviewSerializer
 
 
 class AmenitySerializer(ModelSerializer):
     class Meta:
         model = Amenity
+        fields = (
+            "name",
+            "description",
+        )
+
+
+class RoomListSerializer(ModelSerializer):
+
+    rating = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Room
+        fields = (
+            "pk",
+            "name",
+            "country",
+            "city",
+            "price",
+            "rating",
+            "is_owner",
+        )
+
+    def get_rating(self, room):
+        return room.rating()
+
+    def get_is_owner(self, room):
+        request = self.context["request"]
+        return room.owner == request.user
+
+
+class RoomDetailSerializer(ModelSerializer):
+
+    owner = TinyUserSerializer(
+        read_only=True,
+    )  # read_only = True -> serializer는 owner에 대한 정보 요구x
+    amenities = AmenitySerializer(
+        read_only=True,
+        many=True,
+    )
+    category = CategorySerializer(
+        read_only=True,
+    )
+    rating = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Room
         fields = "__all__"
+
+    def get_rating(self, room):  # 반드시 get_ 을 붙여야 함
+        return room.rating()
+
+    def get_is_owner(self, room):
+        request = self.context["request"]
+        return room.owner == request.user
